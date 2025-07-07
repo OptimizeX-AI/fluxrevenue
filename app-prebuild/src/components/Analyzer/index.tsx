3106234f5a22a77ca5b0f8573bd1a6978ab471b8
OptimizeX-AI <augusto_ccn@hotmail.com>
2025-07-07 06:04:29 -0300

    Delete app-prebuild directory

--- app-prebuild/src/components/Analyzer/index.tsx
+++
@@ -1,6 +0,0 @@
-// src/components/Analyzer/index.tsx
-export { default } from './Analyzer'
-export { default as AnalysisResults } from './AnalysisResults'
-export { default as CSVUploader } from './CSVUploader'
-export { default as FileUpload } from './FileUpload'
-export { default as SiteSelector } from './SiteSelector'
--- app-prebuild/src/components/Dashboard/index.tsx
+++
@@ -1,6 +0,0 @@
-// src/components/Dashboard/index.tsx
-export { default } from './Dashboard'
-export { default as MetricsCard } from './MetricsCard'
-export { default as RecentAnalyses } from './RecentAnalyses'
-export { default as SitesOverview } from './SitesOverview'
-export { default as TrialBanner } from './TrialBanner'
